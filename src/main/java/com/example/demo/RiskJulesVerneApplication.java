70ee6491cc981673fa5ad89385d9ad7acd3501af
verbaaal <verbaaal@gmail.com>
2019-10-14 12:21:07 +0200

    initialisation

---
+++ src/main/java/com/example/demo/RiskJulesVerneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskJulesVerneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskJulesVerneApplication.class, args);
+	}
+
+}
