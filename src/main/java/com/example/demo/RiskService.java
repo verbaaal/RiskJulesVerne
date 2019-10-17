957db760d4d4e2ff345c26f2bf27d1924a66bb4f
Laurelenne <poussin.laurelenne@gmail.com>
2019-10-17 16:58:18 +0200

    add properties

--- src/main/java/com/example/demo/RiskService.java
+++ src/main/java/com/example/demo/RiskService.java
@@ -1,9 +1,27 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class RiskService {
+
+	//instancier le repo
+	@Autowired
+	PlayerRepository playerRepo;
+	@Autowired
+	ContinentRepository continentRepo;
+	@Autowired
+	TerritoryRepository territoryRepo;
+	@Autowired
+	CardTerritoryRepository cardTerritoryRepo;
 	
+	
+		public Player createPlayer(Player player) {
+			return playerRepo.save(player);
+	
+	}
+
 
 }
