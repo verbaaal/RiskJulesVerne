86e4dedefaa8182f7eda79e599e6b72a5dacf248
devNicolasNmk <devnicolasnmk@gmail.com>
2019-10-17 14:48:59 +0200

    repo player modifier

--- src/main/java/com/example/demo/PlayerRepository.java
+++ src/main/java/com/example/demo/PlayerRepository.java
@@ -3,5 +3,7 @@ package com.example.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerRepository extends JpaRepository<Integer, Long> {
+
+	Player save(Player player);
 	
 }
