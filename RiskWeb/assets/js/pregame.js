e953725213a8214af5d4c60b6fabb3728428d98a
devNicolasNmk <devnicolasnmk@gmail.com>
2019-10-24 12:02:27 +0200

    requete get pregame ok

--- RiskWeb/assets/js/pregame.js
+++ RiskWeb/assets/js/pregame.js
@@ -2,30 +2,31 @@
 var urlGet = "http://localhost:8080/getPlayer";
 
 // recuperation de la liste de joueurs dans la bdd
-// var data =fetch(urlGet)
-//     .then(function (response) {
-//         return response.json()
-//     }).then(function (data) {
-//         return data[0].name
-//     })
-// var nbjoueur = document.getElementById('nbPlayer');
-// nbjoueur.innerHTML += data.length;
+let players
+fetch(urlGet)
+    .then(function (response) {
+        return response.json()
+    }).then(function (players) {
+        var nbjoueur = document.getElementById('nbPlayer');
+        nbjoueur.innerHTML += players.length;
+    })
 
 // var test;
-const getPlayer = async function () {
-    try {
-        let response = await fetch(urlGet)
-        if (response.ok) {
-            let datas = await response.json()
-            console.log(datas)
-        } else {
-            console.error('Retour du serveur : ', response.status)
-        }
-    } catch (e){
-        console.log(e)
-    }
-}
-getPlayer()
+// let players;
+// const getPlayer = async function () {
+//     try {
+//         let response = await fetch(urlGet)
+//         if (response.ok) {
+//             players = response.json()
+//         } else {
+//             console.error('Retour du serveur : ', response.status)
+//         }
+//     } catch (e){
+//         console.log(e)
+//     }
+// }
+// getPlayer(); 
+// console.log(players)
 
 
 
